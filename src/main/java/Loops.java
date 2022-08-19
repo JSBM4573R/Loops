426278792ca1199f51c32b64203701c332b58cb2
Juan Sebastian <juan_seb07@hotmail.com>
2022-08-18 23:15:44 -0500

    Se agrega ciclo for

--- src/main/java/Loops.java
+++ src/main/java/Loops.java
@@ -1,2 +1,11 @@
 public class Loops {
+
+    public static void main (String[] args) {
+
+        int num = 0;
+        for (int i = 0; i < 8; i++) {
+            num++;
+            System.out.println(num);
+        }
+    }
 }
