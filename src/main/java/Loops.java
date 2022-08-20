b0cda99ab7f337298ef97aad2b6ef94d0a82a7c1
Juan Sebastian <juan_seb07@hotmail.com>
2022-08-19 23:45:59 -0500

    Se agrega ciclo for each

--- src/main/java/Loops.java
+++ src/main/java/Loops.java
@@ -2,10 +2,16 @@ public class Loops {
 
     public static void main (String[] args) {
 
+        //------Ciclo For
         int num = 0;
         for (int i = 0; i < 8; i++) {
             num++;
-            System.out.println(num);
+            //System.out.println(num);
         }
+
+        //------Ciclo For Each
+        int nums[]={1,2,3,4,5};
+        int sum=0;
+        for (int x: nums) System.out.println(sum+=x);
     }
 }
